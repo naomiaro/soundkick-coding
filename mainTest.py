de8afcabace06194b26f522b302dbbacdfaa9226
Naomi Aro <naomiaro@gmail.com>
2017-01-16 23:32:14 +0000

    bad format test

--- mainTest.py
+++ mainTest.py
@@ -3,10 +3,15 @@ import main as scraper
 
 class Test(unittest2.TestCase):
 
-    def test_parse_venue_location(self):
+    def test_parse_venue_location_good(self):
         [city, venue_name] = scraper.parse_venue_location('LONDON: Brilliant Corners')
         self.assertEqual(city, 'LONDON')
         self.assertEqual(venue_name, 'Brilliant Corners')
+
+    def test_parse_venue_location_bad(self):
+        [city, venue_name] = scraper.parse_venue_location('CHORLEY LITTLE THEATRE, DOLE LANE, CHORLEY, PR7 2RL')
+        self.assertEqual(city, 'CHORLEY LITTLE THEATRE, DOLE LANE, CHORLEY, PR7 2RL')
+        self.assertEqual(venue_name, 'CHORLEY LITTLE THEATRE, DOLE LANE, CHORLEY, PR7 2RL')
 
 
 if __name__ == '__main__':
